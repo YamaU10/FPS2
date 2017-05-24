2fd4c9ed35a69c716d2773933c458775ac4b8a5f
YamamuraYu10 <Yamamu@yamamurayuushounoMacBook-Pro.local>
2017-05-24 21:35:23 +0900

    追尾ロケット弾の追加

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -3,20 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rocket : MonoBehaviour {
-    vp_Grenade vpG;
-	// Use this for initialization
-	void Start () {
-        vpG = GetComponent<vp_Grenade>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-	}
-    void OnCollisionEnter(Collision other)
+    GameObject player;
+    vp_FPPlayerDamageHandler DamegeScript;
+	Transform target;
+	public float speed = 1.0f;
+    public GameObject exprosion;
+    float dis;
+
+    void Start()
     {
-        if(other.gameObject.tag == "Field" || other.gameObject.tag == "Wall"){
-            vpG.LifeTime = 0;
-        }
+        player = GameObject.Find("PA_ArchlightBomber");
+        target = player.transform;
+        DamegeScript = player.GetComponent <vp_FPPlayerDamageHandler>();
     }
+
+    void Update()
+	{
+        //hit
+        dis = Vector3.Distance(target.position, transform.position);
+        if (dis <= 1.0f) {
+            Instantiate(exprosion, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+			//DamegeScript.Damage(DamegePow);
+		}
+
+        //move
+		float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            //DamegeScript.Damage(DamegePow);
+		}
+		Destroy(gameObject);
+	}
 }
